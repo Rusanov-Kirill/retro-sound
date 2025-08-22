c46c5f367fef4940501f49a81e63e67eeb863027
Rusanov-Kirill <rusanovkirill39@gmail.com>
2025-08-23 03:06:01 +0600

    feat: add window close functionality to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,14 @@ import Logo from '../Logo/Logo';
 import styles from './Header.module.scss';
 
 const Header = () => {
+    const handleClose = async () => await window.electronApp.window.close();
+
     return (
         <div className={styles.wrapper}>
             <div className={styles['drag-area']}></div>
             <Button border={true} background={true}>+</Button>
             <Logo />
-            <Button border={true} background={true}>&times;</Button>
+            <Button border={true} background={true} onClick={handleClose}>&times;</Button>
         </div>
     )
 }
