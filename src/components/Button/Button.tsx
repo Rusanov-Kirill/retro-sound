e989368582d4028c42ce156e96900e21a7c19b71
Rusanov-Kirill <rusanovkirill39@gmail.com>
2025-08-23 03:04:45 +0600

    types: modify ButtonProps interface

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,18 @@ interface ButtonProps {
     children: React.ReactNode;
     border?: boolean;
     background?: boolean;
+    onClick?: () => Promise<void>;
 }
 
 
-const Button = ( { children, border = false, background = false }: ButtonProps) => {
+const Button = ( { children, border = false, background = false, onClick }: ButtonProps) => {
     return (
-        <button className={`${styles.button} ${border ? styles.border : ''} ${background ? styles['background-color'] : ''}`}>{children}</button>
+        <button 
+            className={`${styles.button} ${border ? styles.border : ''} ${background ? styles['background-color'] : ''}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
     )
 }
 
